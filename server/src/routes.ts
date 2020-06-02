848a391c24f081f24d5b30684424e23a14c81c46
Hans <hansalmeida@hotmail.com>
2020-06-02 14:03:08 -0300

    Add Point creation
    * incomplete Image, Items upload

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,39 @@ routes.get("/items", async (req: Request, res: Response) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`
     }
   })
 
   return res.json(serializedItems)
+})
+
+routes.post("/points", async (req: Request, res: Response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body
+
+  await knex("points").insert({
+    image: "nonexistent-image",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  })
+
+  return res.json({ success: true })
 })
 
 export default routes
