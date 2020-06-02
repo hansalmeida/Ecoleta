f1e123b9569c92af5fb48762d20ab282f855587c
Hans <hansalmeida@hotmail.com>
2020-06-02 13:50:17 -0300

    List all items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express'
+import knex from "./database/connection"
 
 const routes = express.Router()
 
-routes.get("/", (req: Request, res: Response) => {
-  res.json({
-    name: "ハンス",
-    message: "Next Level Weekへようこそ！"
+routes.get("/items", async (req: Request, res: Response) => {
+  const items = await knex("items").select("*")
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
   })
+
+  return res.json(serializedItems)
 })
 
 export default routes
