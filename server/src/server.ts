0f24dceb0b3dac259af7e9a6bf7e724988fa1853
Hans <hansalmeida@hotmail.com>
2020-06-02 15:09:40 -0300

    Add Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response } from "express"
+import express from "express"
+import cors from "cors"
 import routes from "./routes"
 import path from 'path'
 
 const app = express()
 
+// Express middlewares
+app.use(cors())
 app.use(express.json())
-
 app.use(routes)
-
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")))
 
 const port = 3333
