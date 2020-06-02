8377dff34f2f84405f0c2d82ad3892ef353fa08d
Hans <hansalmeida@hotmail.com>
2020-06-02 13:42:48 -0300

    Add serve static files path

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express"
 import routes from "./routes"
+import path from 'path'
 
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")))
 
 const port = 3333
 app.listen(port, () => console.log(`listening on ${port}`))
