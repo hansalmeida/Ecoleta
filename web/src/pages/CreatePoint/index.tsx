fc6e59ba670429d3cc586d4c85d0a22addd247d8
Hans <hansalmeida@hotmail.com>
2020-06-03 12:28:34 -0300

    Add Create-point form layout

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,94 @@
 import React from "react"
 import "./styles.scss"
+import logo from "../../assets/logo.svg"
+import { Link } from "react-router-dom"
+import { FiArrowLeft } from "react-icons/fi"
 
 interface ICreatePointProps {}
 
 const CreatePoint: React.FC<ICreatePointProps> = () => {
   return (
-    <div id="CreatePoint-component">
-      <h1>CreatePoint Component</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" />
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+      <form>
+        <h1>
+          Cadastro do <br /> ponto de coleta
+        </h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-Mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Itens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            {[1, 2, 3, 4, 5, 6].map((item) => {
+              return (
+                <li>
+                  <img
+                    src="http://localhost:3333/uploads/oleo.svg"
+                    alt="oleo"
+                  />
+                  <span>Óleo de cozinha</span>
+                </li>
+              )
+            })}
+          </ul>
+        </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </form>
     </div>
   )
 }
