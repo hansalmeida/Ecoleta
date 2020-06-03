6bc98cd9f7102e895680f2327a8ae57d5eb47ce2
Hans <hansalmeida@hotmail.com>
2020-06-03 08:55:56 -0300

    Clear React root setup files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import "./App.scss"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>ハローハンス</h1>
-      </header>
+      <h1>ハローハンス</h1>
     </div>
   )
 }
