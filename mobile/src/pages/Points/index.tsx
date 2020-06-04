32ad31ce3ab39afb23d847d64e6641b03376fcce
Hans <hansalmeida@hotmail.com>
2020-06-04 13:48:13 -0300

    Add Item select logic

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -16,6 +16,7 @@ interface IItems {
 
 const Points = () => {
   const [items, setItems] = useState<IItems[]>([])
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
   const navigation = useNavigation()
 
   useEffect(() => {
@@ -29,6 +30,17 @@ const Points = () => {
   }
   function handleNavigateToDetail() {
     navigation.navigate("Detail")
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex((item) => item === id)
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id)
+      setSelectedItems(filteredItems)
+    } else {
+      setSelectedItems([...selectedItems, id])
+    }
   }
 
   return (
@@ -86,8 +98,12 @@ const Points = () => {
           {items.map((item) => (
             <TouchableOpacity
               key={String(item.id)}
-              style={styles.item}
-              onPress={() => {}}
+              style={[
+                styles.item,
+                selectedItems.includes(item.id) ? styles.selectedItem : {},
+              ]}
+              onPress={() => handleSelectItem(item.id)}
+              activeOpacity={0.7}
             >
               <SvgUri width={42} height={42} uri={item.image_url} />
               <Text style={styles.itemTitle}>{item.title}</Text>
