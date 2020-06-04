a7ad51fbab63df1a073d4965e7af62309df7c419
Hans <hansalmeida@hotmail.com>
2020-06-04 10:16:11 -0300

    Add Routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import Home from "./src/pages/Home"
 import { AppLoading } from "expo"
 import { StatusBar } from "react-native"
 import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto"
 import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu"
+import Routes from "./src/routes"
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +23,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </>
   )
 }
