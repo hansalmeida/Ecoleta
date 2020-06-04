06cde1ed647f469e2f0e0a8d81ec0ef67ef77894
Hans <hansalmeida@hotmail.com>
2020-06-04 14:01:59 -0300

    Add current position on map

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { View, StyleSheet, Text, Image } from "react-native"
+import { View, StyleSheet, Text, Image, Alert } from "react-native"
 import Constants from "expo-constants"
 import { TouchableOpacity, ScrollView } from "react-native-gesture-handler"
 import { Feather as Icon } from "@expo/vector-icons"
@@ -7,6 +7,7 @@ import { useNavigation } from "@react-navigation/native"
 import MapView, { Marker } from "react-native-maps"
 import { SvgUri } from "react-native-svg"
 import api from "../../services/api"
+import * as Location from "expo-location"
 
 interface IItems {
   id: number
@@ -17,12 +18,36 @@ interface IItems {
 const Points = () => {
   const [items, setItems] = useState<IItems[]>([])
   const [selectedItems, setSelectedItems] = useState<number[]>([])
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ])
+
   const navigation = useNavigation()
 
   useEffect(() => {
     api.get("items").then((response) => {
       setItems(response.data)
     })
+  }, [])
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await Location.requestPermissionsAsync()
+      if (status !== "granted") {
+        Alert.alert(
+          "Ooops...",
+          "Precisamos de sua permissão para obter sua localização"
+        )
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+
+      const { latitude, longitude } = location.coords
+
+      setInitialPosition([latitude, longitude])
+    }
+    loadPosition()
   }, [])
 
   function handleNavigateBack() {
@@ -56,35 +81,38 @@ const Points = () => {
         </Text>
 
         <View style={styles.mapContainer}>
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: -12.9335002,
-              longitude: -38.4113509,
-              latitudeDelta: 0.014,
-              longitudeDelta: 0.014,
-            }}
-          >
-            <Marker
-              style={styles.mapMarker}
-              onPress={handleNavigateToDetail}
-              coordinate={{
-                latitude: -12.9335002,
-                longitude: -38.4113509,
+          {initialPosition[0] !== 0 && (
+            <MapView
+              style={styles.map}
+              loadingEnabled={initialPosition[0] === 0}
+              initialRegion={{
+                latitude: initialPosition[0],
+                longitude: initialPosition[1],
+                latitudeDelta: 0.014,
+                longitudeDelta: 0.014,
               }}
             >
-              <View style={styles.mapMarkerContainer}>
-                <Image
-                  style={styles.mapMarkerImage}
-                  source={{
-                    uri:
-                      "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-                  }}
-                />
-                <Text style={styles.mapMarkerTitle}>Mercado</Text>
-              </View>
-            </Marker>
-          </MapView>
+              <Marker
+                style={styles.mapMarker}
+                onPress={handleNavigateToDetail}
+                coordinate={{
+                  latitude: -12.9335002,
+                  longitude: -38.4113509,
+                }}
+              >
+                <View style={styles.mapMarkerContainer}>
+                  <Image
+                    style={styles.mapMarkerImage}
+                    source={{
+                      uri:
+                        "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+                    }}
+                  />
+                  <Text style={styles.mapMarkerTitle}>Mercado</Text>
+                </View>
+              </Marker>
+            </MapView>
+          )}
         </View>
       </View>
       <View style={styles.itemsContainer}>
