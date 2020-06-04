5a289a591461480a7f085de37b807b569d87d587
Hans <hansalmeida@hotmail.com>
2020-06-04 09:44:17 -0300

    Complete Home layout

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,11 +1,36 @@
 import React from "react"
-import { View, Image, StyleSheet } from "react-native"
+import { View, Image, StyleSheet, Text, ImageBackground } from "react-native"
+import { RectButton } from "react-native-gesture-handler"
+import { Feather as Icon } from "@expo/vector-icons"
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require("../../assets/logo.png")} />
-    </View>
+    <ImageBackground
+      source={require("../../assets/home-background.png")}
+      imageStyle={{ width: 274, height: 368 }}
+      style={styles.container}
+    >
+      <View style={styles.main}>
+        <Image source={require("../../assets/logo.png")} />
+        <Text style={styles.title}>
+          Seu marketplace para coleta de resíduos
+        </Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#fff" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
   )
 }
 
@@ -13,6 +38,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+    backgroundColor: "#f0f0f5",
   },
 
   main: {
