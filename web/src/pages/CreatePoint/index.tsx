3aabe76ef3819b76d8628cbaa4ca2e3044c86bf1
Hans <hansalmeida@hotmail.com>
2020-06-05 13:38:59 -0300

    Update form to accept image

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -42,6 +42,7 @@ const CreatePoint: React.FC<ICreatePointProps> = () => {
     whatsapp: "",
   })
   const [selectedItems, setSelectedItems] = useState<number[]>([])
+  const [selectedFile, setSelectedFile] = useState<File>()
   const history = useHistory()
 
   useEffect(() => {
@@ -118,15 +119,18 @@ const CreatePoint: React.FC<ICreatePointProps> = () => {
     const [latitude, longitude] = selectedPosition
     const items = selectedItems
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
+    const data = new FormData()
+
+    data.append("name", name)
+    data.append("email", email)
+    data.append("whatsapp", whatsapp)
+    data.append("uf", uf)
+    data.append("city", city)
+    data.append("latitude", String(latitude))
+    data.append("longitude", String(longitude))
+    data.append("items", items.join(","))
+    if (selectedFile) {
+      data.append("image", selectedFile)
     }
 
     try {
@@ -153,7 +157,7 @@ const CreatePoint: React.FC<ICreatePointProps> = () => {
           Cadastro do <br /> ponto de coleta
         </h1>
 
-        <Dropzone />
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
