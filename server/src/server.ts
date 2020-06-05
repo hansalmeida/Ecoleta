ff4bfee9513a0ac25e611dd7514d1320150edabb
Hans <hansalmeida@hotmail.com>
2020-06-05 14:10:26 -0300

    Configure server to use json validation errors

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import routes from "./routes"
 import path from 'path'
+import { errors } from 'celebrate'
 
 const app = express()
 
@@ -9,8 +10,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")))
 app.use("/uploads/pointPictures", express.static(path.resolve(__dirname, "..", "uploads", "pointPictures")))
 
+app.use(errors())
 const port = 3333
 app.listen(port, () => console.log(`listening on ${port}`))
